cdf0a4bb530bb5802bf32de61a4ea7e901124707
DGuskov <denis180391@yandex.ru>
2023-06-22 21:53:06 +0300

    Added task00, task01

---
+++ Sem_1/task_00/Program.cs
@@ -0,0 +1,13 @@
+﻿//напишите программу, которая на входе принимает число и выдает его квадрат/
+// Например:
+// 4 -> 16; -3 ->9; -7 -> 49.
+
+Console.Clear();
+Console.Write("Введите число а: ");
+int a = int.Parse(Console.ReadLine());
+// 1 metod
+int result1 = a*a;
+Console.WriteLine($"способ 1 -> Квадрат числа {a} равен {result1}");
+// 2 metod
+int result2 = Convert.ToInt32(Math.Pow(a,2));
+Console.WriteLine($"способ 2 -> Квадрат числа {a} равен {result2}");
---
+++ Sem_1/task_01/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// написать программу, которая на вход принимает два числа и проверяет, 
+// является ли первое число квадратом второго.
+// например:
+// 25, 5 -> да;  
+// 2, 10 -> нет;  
+// 9, -3 -> да;  
+// -3, 9 -> нет.
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int a Console.ReadLine()
