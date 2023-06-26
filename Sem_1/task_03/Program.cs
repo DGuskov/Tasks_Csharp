c0edbef9dafddd0013e4b71c5a2e36ea711f536c
DGuskov <denis180391@yandex.ru>
2023-06-26 18:42:09 +0300

    added task03

---
+++ Sem_1/task_03/Program.cs
@@ -0,0 +1,12 @@
+﻿// написать программу которая на вход принимает одно чисто (N), 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.Clear();
+Console.WriteLine("Введите натуральное число: ");
+int N = int.Parse(Console.ReadLine());
+int index = -N;
+while (index <= N)
+{
+    Console.Write($"{index}, ");
+    index++;
+}
